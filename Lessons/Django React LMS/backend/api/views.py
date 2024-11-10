02ad462e308ebf77ae9ff2d27020c2a6c45c7106
Re1 <revanaxun559@gmail.com>
2024-11-10 21:41:52 +0400

    Registration Seriallizer view and Pass reset email verification function

--- Lessons/Django React LMS/backend/api/views.py
+++ Lessons/Django React LMS/backend/api/views.py
@@ -1,9 +1,11 @@
+import random
 from django.shortcuts import render
 from api import serializer as api_serializer
 from userauths.models import User, Profile
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -13,3 +15,28 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = api_serializer.RegisterSerializer
 # Create your views here.
+
+def generate_random_otp(Lenght=7):
+    otp = ''.join([str(random.randint(0, 9)) for i in range(Lenght)])
+    return otp
+
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def get_object(self):
+        email = self.kwargs.get('email')
+        user = User.objects.filter(email=email).first()
+        if user:
+
+            uuidb64 = user.pk
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh.access_token)
+
+            user.refresh_token = refresh_token
+            user.otp = generate_random_otp()            
+            user.save()
+            link = f"http://localhost:5173/create-new-password/?otp={user.otp}&uuidb64={uuidb64}&=refresh_token{refresh_token}"
+            print("print link =======", link)
+        
+        return user
