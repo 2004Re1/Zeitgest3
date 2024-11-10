c4d59092a62e9251b64954586108984b9016c577
Re1 <revanaxun559@gmail.com>
2024-11-10 17:22:35 +0400

    Custom User and Profile Model

--- Lessons/Django React LMS/backend/backend/settings.py
+++ Lessons/Django React LMS/backend/backend/settings.py
@@ -124,12 +124,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]   # Path to static files
 
 STATICROOT = BASE_DIR / "templates" # Path to templates
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'userauths.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
