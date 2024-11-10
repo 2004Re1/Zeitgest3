cd22ff0eb831c7cffb74eebf1bb463c51237dd61
Re1 <revanaxun559@gmail.com>
2024-11-10 21:55:59 +0400

    Password Change Function

--- Lessons/Django React LMS/backend/api/views.py
+++ Lessons/Django React LMS/backend/api/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from api import serializer as api_serializer
 from userauths.models import User, Profile
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -40,3 +41,24 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
             print("print link =======", link)
         
         return user
+    
+
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        otp = request.data['otp']
+        uuidb64 = request.data['uuidb64']
+        password = request.data['password']
+
+        user = User.objects.get(id=uuidb64, otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp = ""
+            user.save()
+
+            return Response({"message": "Password changed successfully."}, status=status.HTTP_200_CREATED)
+        else:
+            return Response({"message": "User Does Not Exists"}, status=status.HTTP_404_NOT_FOUND)
+           
