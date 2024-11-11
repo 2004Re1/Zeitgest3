a6b1f1d2bcd406ee28d49a968371a46ad74a6230
Re1 <revanaxun559@gmail.com>
2024-11-11 16:41:47 +0400

    Forgot Password system is done

--- Lessons/Django React LMS/frontend/src/App.jsx
+++ Lessons/Django React LMS/frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PrivateRoute from "./layouts/PrivateRoute";
 import Register from "../src/views/auth/Register";
 import Login from "../src/views/auth/Login";
 import Logout from "../src/views/auth/Logout";
+import ForgotPassword from "./views/auth/ForgotPassword";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register/" element={<Register />} />
           <Route path="/login/" element={<Login />} />
           <Route path="/logout/" element={<Logout />} />
+          <Route path="/forgot-password/" element={<ForgotPassword />} />
         </Routes>
       </MainWrapper>
     </BrowserRouter>
