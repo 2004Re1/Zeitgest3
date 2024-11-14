e0576a7b04158868cc916047f95e615483d178a9
Mehralizada <gulanemehrelizade97@gmail.com>
2024-11-14 05:54:45 -0800

    Add Course, Category, and Teacher models in Django

--- Lessons/Django React LMS/backend/api/models.py
+++ Lessons/Django React LMS/backend/api/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+from userauths.models import User, Profile
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+LANGUAGE = (
+    ("English", "English"),
+    ("Spanish", "Spanish"),
+    ("French", "French"),
+)
+
+LEVEL = (
+    ("Beginner", "Beginner"),
+    ("Intemediate", "Intemediate"),
+    ("Advanced", "Advanced"),
+)
+
+TEACHER_STATUS = (
+    ("Draft", "Draft"),
+    ("Disabled", "Disabled"),
+    ("Published", "Published"),
+)
+
+PLATFORM_STATUS = (
+    ("Review", "Review"),
+    ("Disabled", "Disabled"),
+    ("Rejected", "Rejected"),
+    ("Draft", "Draft"),
+    ("Published", "Published"),
+)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="course-file", blank=True, null=True, default="default.jpg")
+    full_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    linkedin = models.URLField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    def students(self):
+        return CartOrderItem.objects.filter(teacher=self)
+    
+    def courses(self):
+        return Course.objects.filter(teacher=self)
+    
+    def review(self):
+        return Course.objects.filter(teacher=self).count()
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="course-file", default="category.jpg", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+    
+    def course_count(self):
+        return Course.objects.filter(category=self).count()
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) 
+        super(Category, self).save(*args, **kwargs)
+
+class Course(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="course-file", blank=True, null=True)
+    image = models.FileField(upload_to="course-file", blank=True, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    language = models.CharField(choices=LANGUAGE, default="English", max_length=100)
+    level = models.CharField(choices=LEVEL, default="Beginner", max_length=100)
+    platform_status = models.CharField(choices=PLATFORM_STATUS, default="Published", max_length=100)
+    teacher_course_status = models.CharField(choices=TEACHER_STATUS, default="Published", max_length=100)
+    featured = models.BooleanField(default=False)
+    course_id = ShortUUIDField(unique=True, length=6, max_length=20, alphabet="1234567890")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + str(self.pk)
+        super(Course, self).save(*args, **kwargs)
