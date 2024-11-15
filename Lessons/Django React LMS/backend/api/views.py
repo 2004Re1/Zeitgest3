07415bdf0eee91c071743de2ccd09c025a4cb052
Mehralizada <gulanemehrelizade97@gmail.com>
2024-11-15 02:22:27 -0800

    PayPal Access Token in Django Rest framework

--- Lessons/Django React LMS/backend/api/views.py
+++ Lessons/Django React LMS/backend/api/views.py
@@ -402,3 +402,16 @@ class StripeCheckoutAPIView(generics.CreateAPIView):
         except stripe.error.StripeError as e:
             return Response({"message": f"Something went wrong when trying to make payment. Error: {str(e)}"})
 
+    
+    def get_access_token(client_id, secret_key):
+    token_url = "https://api.sandbox.paypal.com/v1/oauth2/token"
+    data = {'grant_type': 'client_credentials'}
+    auth = (client_id, secret_key)
+    response = requests.post(token_url, data=data, auth=auth)
+
+    if response.status_code == 200:
+        print("Access TOken ====", response.json()['access_token'])
+        return response.json()['access_token']
+    else:
+        raise Exception(f"Failed to get access token from paypal {response.status_code}")
+
