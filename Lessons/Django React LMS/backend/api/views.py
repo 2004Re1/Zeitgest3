89c3bd79c498a49448aaa053bc8e60fc4ae0d4e6
Mehralizada <gulanemehrelizade97@gmail.com>
2024-11-14 23:03:36 -0800

    Add Cart API endpoint for creating and updating cart items with tax calculation based on country

--- Lessons/Django React LMS/backend/api/views.py
+++ Lessons/Django React LMS/backend/api/views.py
@@ -136,3 +136,64 @@ class CourseDetailAPIView(generics.RetrieveAPIView):
         slug = self.kwargs['slug']
         course = api_models.Course.objects.get(slug=slug, platform_status="Published", teacher_course_status="Published")
         return course
+
+class CartAPIView(generics.CreateAPIView):
+    queryset = api_models.Cart.objects.all()
+    serializer_class = api_serializer.CartSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        course_id = request.data['course_id']  
+        user_id = request.data['user_id']
+        price = request.data['price']
+        country_name = request.data['country_name']
+        cart_id = request.data['cart_id']
+
+        print("course_id ==========", course_id)
+
+        course = api_models.Course.objects.filter(id=course_id).first()
+        
+        if user_id != "undefined":
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = None
+
+        try:
+            country_object = api_models.Country.objects.filter(name=country_name).first()
+            country = country_object.name
+        except:
+            country_object = None
+            country = "United States"
+
+        if country_object:
+            tax_rate = country_object.tax_rate / 100
+        else:
+            tax_rate = 0
+
+        cart = api_models.Cart.objects.filter(cart_id=cart_id, course=course).first()
+
+        if cart:
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Updated Successfully"}, status=status.HTTP_200_OK)
+
+        else:
+            cart = api_models.Cart()
+
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Created Successfully"}, status=status.HTTP_201_CREATED)
